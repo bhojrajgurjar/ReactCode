add2259b4ce4f1336219e5c37a90235cc194d46b
bhojrajgurjar <bgurjar2000@gmail.com>
2024-02-06 19:31:29 +0530

    first create

---
+++ 01basicReact/src/App.jsx
@@ -0,0 +1,13 @@
+import MyFun from "./MyFun";
+
+function App() {
+  
+  return (
+    <>
+        <h2>Chai Aur Code || Bhojraj Gurjar</h2>
+        <MyFun />
+    </>
+  );
+}
+
+export default App
---
+++ 01basicReact/src/MyFun.jsx
@@ -0,0 +1,6 @@
+
+function MyFun(){
+    return ( <h3>This is my function</h3>);
+}
+
+export default MyFun;
